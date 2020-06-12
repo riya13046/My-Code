a2500a0a7e0bc61cec7d90f96c450bf31ba3ffc5
riya13046 <60398743+riya13046@users.noreply.github.com>
2020-06-12 16:14:11 +0530

    Update thanos sort.py

--- thanos sort.py
+++ thanos sort.py
@@ -1,14 +1,18 @@
+
 def fun(b):
     global m
     if b==sorted(b):
+        //we can also use the max function of python instead of this code
         if len(b)>m:
             m=len(b)
     else:
+        // Here we can also take the elements of splitted list using for loop but it will unnecessarily increase the complexity of program
         l=b[:len(b)//2:]
         l1=b[len(b)//2:len(b):]
         fun(l)
         fun(l1)
 a=int(input())
+//To take space separated elements as input
 b=list(map(int,input().split()))
 m=0
 fun(b)
