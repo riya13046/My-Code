e35725a8823fc8be496da773d7c734c19d4ed81d
riya13046 <60398743+riya13046@users.noreply.github.com>
2020-06-12 16:50:19 +0530

    Deletion of 'N' elements from a array.

---
+++ 'N' deletion.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=input().split()
+c=0
+# to remove n number of elements
+for i in range(n):
+    a.remove(a[0])
+# to remove elements which are repeating
+b=set(a)
+# to count elements remaining
+for i in b:
+    c=c+1
+print(c)
+
+    
